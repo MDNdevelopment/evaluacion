33c0015dac86da339cf025a83dee129f79c618a5
Juan <juanluislauretta@gmail.com>
2025-07-03 12:32:16 -0400

    fix: fixed vitest installation

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import path from "path";
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,8 +11,14 @@ export default defineConfig({
     },
   },
   test: {
-    environment: "jsdom",
     globals: true,
-    // add more options as needed
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup.ts",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: ["src/tests/**/*", "src/**/*.d.ts"],
+    },
   },
 });
